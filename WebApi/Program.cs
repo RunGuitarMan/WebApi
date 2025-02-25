c55f648685872afa0d9a6191220c0d0fbc6c9758
Aleksey Ulyanovskiy <aleksey@ulyanovskiy.com>
2025-02-25 21:00:22 +0300

    add UseDeveloperExceptionPage

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,16 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService("My App", serviceInstanceId: Environment.MachineName))
     .WithMetrics(meterBuilder => meterBuilder
-            .AddAspNetCoreInstrumentation()
-            .AddHttpClientInstrumentation()
-            .AddRuntimeInstrumentation()
-            .AddProcessInstrumentation()
-            .AddPrometheusExporter()
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddProcessInstrumentation()
+        .AddPrometheusExporter()
     );
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseOpenTelemetryPrometheusScrapingEndpoint();
 app.MapControllers();
